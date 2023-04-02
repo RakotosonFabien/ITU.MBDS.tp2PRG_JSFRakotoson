a6468f50610e3c60039b267faa10d0526f2a984c
RakotosonFabien <rakotosonfabienmaminirina@gmail.com>
2023-04-02 22:44:53 +0300

    Creation index, affichages, formulaires

--- src/main/java/itu/mbds/fabien/tp2prg_jsfrakotoson/jsf/Bean.java
+++ src/main/java/itu/mbds/fabien/tp2prg_jsfrakotoson/jsf/Bean.java
@@ -35,6 +35,6 @@ public class Bean {
         return l;
     }
     public String afficher() {
-        return "affichage_3?nb="+ nombre + "&amp;faces-redirect=true";
+        return "affichage__3?nb="+ nombre + "&amp;faces-redirect=true";
     }
 }
